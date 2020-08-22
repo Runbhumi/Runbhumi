742b52715f2558cec14334aafc67b34347ec84d8
Hayat Tamboli <hayat.tamboli@gmail.com>
2020-08-23 01:17:59 +0530

    🧪added a basic test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,21 +10,29 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:Runbhumi/main.dart';
 
+// void main() {
+//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+//     // Build our app and trigger a frame.
+//     await tester.pumpWidget(MyApp());
+
+//     // Verify that our counter starts at 0.
+//     expect(find.text('0'), findsOneWidget);
+//     expect(find.text('1'), findsNothing);
+
+//     // Tap the '+' icon and trigger a frame.
+//     await tester.tap(find.byIcon(Icons.add));
+//     await tester.pump();
+
+//     // Verify that our counter has incremented.
+//     expect(find.text('0'), findsNothing);
+//     expect(find.text('1'), findsOneWidget);
+//   });
+// }
+
+
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('app should work', (tester) async {
+    await tester.pumpWidget(new MyApp());
+    expect(find.text('New here? Create an account'), findsOneWidget);
   });
 }

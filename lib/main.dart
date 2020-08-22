05c73616e46605f6cedf2d67e42047c9bd92d079
Hayat Tamboli <hayat.tamboli@gmail.com>
2020-08-22 20:47:02 +0530

    🧹 clean login page ui and removed all  warnings

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:runbhumi_ui/authentication/loginPage.dart';
+import 'authentication/LoginPage.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: loginPage(),
+      home: LoginPage(),
     );
   }
 }

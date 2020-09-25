6941761a309690422b75166c92901764ffa8972f
Manas Gupta <manasgupta1820@gmail.com>
2020-09-25 19:09:55 +0530

    💨 Improved Functionality"

--- lib/models/User.dart
+++ lib/models/User.dart
@@ -1,40 +1,21 @@
-class UserProfile {
-  String userId;
+class UserProfile extends Info {
+  final String userId;
+  List<String> friendsId;
+  List<String> teamId;
+  List<String> eventsID;
+  UserProfile({this.userId});
+}
+
+class Info {
   String username;
   String name;
+  DateTime dob;
   String profileImage;
   String location;
   String address;
   String phoneNumber;
-  List<String> friendsId;
-  List<String> teamId;
-  List<String> eventsId;
-
-  UserProfile.newuser(userId, username, name, profileImage, phoneNumber) {
-    this.userId = userId;
-    this.username = username;
-    this.name = name;
-    this.profileImage = profileImage;
-    this.location = '';
-    this.address = '';
-    this.phoneNumber = '';
-    this.friendsId = [];
-    this.teamId = [];
-    this.eventsId = [];
-  }
-
-  Map<String, dynamic> toJson() => {
-        'userId': userId,
-        'username': username,
-        'name': name,
-        'profileImage': profileImage,
-        'location': location,
-        'address': address,
-        'phoneNumber': phoneNumber,
-        'friendsId': friendsId,
-        'teamId': teamId,
-        'eventsId': eventsId
-      };
 }
 
-// Notifications can be taken as a seperate calss
+class Nortifications {
+  // This is a incomplete class mohit will decide on this !
+}

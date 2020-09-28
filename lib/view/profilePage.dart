a0b3845fc9b9c1e4ad611ed565f857821040c853
Hayat Tamboli <hayat.tamboli@gmail.com>
2020-09-28 13:09:13 +0530

    📃added grid in friends

--- lib/view/profilePage.dart
+++ lib/view/profilePage.dart
@@ -287,7 +287,20 @@ class ProfileBody extends StatelessWidget {
                 children: [
                   PlaceholderWidget(),
                   PlaceholderWidget(),
-                  PlaceholderWidget(),
+                  GridView.count(
+                    // Create a grid with 2 columns. If you change the scrollDirection to
+                    // horizontal, this produces 2 rows.
+                    crossAxisCount: 2,
+                    // Generate 100 widgets that display their index in the List.
+                    children: List.generate(100, (index) {
+                      return Center(
+                        child: Text(
+                          'Item $index',
+                          style: Theme.of(context).textTheme.headline5,
+                        ),
+                      );
+                    }),
+                  ),
                 ],
               ),
             ),

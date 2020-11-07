ef8ec8cf4c568f8417bc71c4dd9061a5daa9da7d
Manas Gupta <manasgupta1820@gmail.com>
2020-11-07 17:26:31 +0530

    🛠 Messages Refactored

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -4,12 +4,13 @@ class Message {
   DateTime dateTime;
   String sentby;
   String message;
+  // List<dynamic> reaction;
 
   Message({
     this.dateTime,
     this.sentby,
     this.message,
-  });
+  }); //this.reaction});
 
   Message.newMessage(
     DateTime dateTime,
@@ -19,12 +20,28 @@ class Message {
     this.dateTime = dateTime;
     this.sentby = sentby;
     this.message = message;
+    // this.reaction = [0, 0, 0, 0, 0];
   }
 
-  Map<String, dynamic> toJson() =>
-      {'dateTime': dateTime, 'sentby': sentby, 'message': message};
-  Message.fromSnapshot(DocumentSnapshot snapshot)
-      : dateTime = snapshot.data()['dateTime'],
-        sentby = snapshot.data()['sentby'],
-        message = snapshot.data()['message'];
+  Map<String, dynamic> toJson() => {
+        'dateTime': dateTime,
+        'sentby': sentby,
+        'message': message,
+        // 'reaction': reaction
+      };
+
+  // Message.fromSnapshot(DocumentSnapshot snapshot)
+  //     : dateTime = snapshot.data()['dateTime'],
+  //       sentby = snapshot.data()['sentby'],
+  //       message = snapshot.data()['message'],
+  //       reaction = snapshot.data()['reaction'];
+
+  factory Message.fromJson(QueryDocumentSnapshot data) {
+    var parsedJson = data.data();
+    return Message(
+        dateTime: parsedJson['dateTime'].toDate(),
+        sentby: parsedJson['sentby'],
+        message: parsedJson['message']);
+    //    reaction: parsedJson['reaction']);
+  }
 }

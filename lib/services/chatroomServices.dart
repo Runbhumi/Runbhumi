98e001d397c271fa608b2a278d2e44c0ffff5505
SbangariGIT <56751089+sbangariGIT@users.noreply.github.com>
2020-11-15 16:42:14 +0530

    timeStamp, sentByName available for Msg Tile

--- lib/services/chatroomServices.dart
+++ lib/services/chatroomServices.dart
@@ -26,25 +26,25 @@ class ChatroomService {
     });
   }
 
-  void sendNewMessage(
-      DateTime dateTime, String sentby, String message, chatRoomId) {
+  void sendNewMessage(DateTime dateTime, String sentby, String message,
+      String sentByName, chatRoomId) {
     FirebaseFirestore.instance
         .collection("DirectChats")
         .doc(chatRoomId)
         .collection("chats")
-        .add(Message.newMessage(dateTime, sentby, message).toJson())
+        .add(Message.newMessage(dateTime, sentby, message, sentByName).toJson())
         .catchError((e) {
       print(e.toString());
     });
   }
 
-  void sendNewMessageTeam(
-      DateTime dateTime, String sentby, String message, teamId) {
+  void sendNewMessageTeam(DateTime dateTime, String sentby, String message,
+      String sentByName, teamId) {
     FirebaseFirestore.instance
         .collection("teams")
         .doc(teamId)
         .collection("chats")
-        .add(Message.newMessage(dateTime, sentby, message).toJson())
+        .add(Message.newMessage(dateTime, sentby, message, sentByName).toJson())
         .catchError((e) {
       print(e.toString());
     });

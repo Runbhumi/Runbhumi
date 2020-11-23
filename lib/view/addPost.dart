f109a0195d73501d0c5a69e25e7e2e5a176d1f78
Hayat Tamboli <hayat.tamboli@gmail.com>
2020-11-23 14:33:04 +0530

    ✨added type and status

--- lib/view/addPost.dart
+++ lib/view/addPost.dart
@@ -18,11 +18,11 @@ class _AddPostState extends State<AddPost> {
   String _chosenSport;
   String _chosenPurpose;
   double _maxMembers = 0;
+  String _status = 'public';
+  String _type = 'team';
   TextEditingController _locationController = new TextEditingController();
   TextEditingController _datetime = new TextEditingController();
   TextEditingController _nameController = new TextEditingController();
-  TextEditingController _typeController = new TextEditingController();
-  TextEditingController _statusController = new TextEditingController();
   final PageController _addPostPageController = PageController(initialPage: 0);
 
   @override
@@ -95,53 +95,73 @@ class _AddPostState extends State<AddPost> {
       min: 0,
       max: 50,
       label: _maxMembers.toString(),
-      divisions: 25,
+      divisions: 50,
+    );
+    var publicRadio = RadioListTile(
+      groupValue: _status,
+      title: Text('Public'),
+      value: 'public',
+      onChanged: (val) {
+        setState(() => _status = val);
+      },
+    );
+    var privateRadio = RadioListTile(
+      groupValue: _status,
+      title: Text('Private'),
+      value: 'private',
+      onChanged: (val) {
+        setState(() => _status = val);
+      },
+    );
+    var teamRadio = RadioListTile(
+      groupValue: _type,
+      title: Text('Team'),
+      value: 'team',
+      onChanged: (val) {
+        setState(() => _type = val);
+      },
+    );
+    var individualRadio = RadioListTile(
+      groupValue: _type,
+      title: Text('Individual'),
+      value: 'individual',
+      onChanged: (val) {
+        setState(() => _type = val);
+      },
     );
     return Scaffold(
-        // body: NestedScrollView(
-        // headerSliverBuilder: addPostSliverAppBar,
-        appBar: AppBar(
-          leading: BackButton(),
-          title: buildTitle(context, "Add Post"),
-        ),
-        body: PageView(
-          controller: _addPostPageController,
-          children: [
-            Page1(
-                addpostkey: _addpostkey,
-                nameController: _nameController,
-                theme: theme,
-                sportsList: sportsList,
-                purposeList: purposeList,
-                datetime: _datetime,
-                locationController: _locationController,
-                slider: slider,
-                chosenSport: _chosenSport,
-                chosenPurpose: _chosenPurpose,
-                maxMembers: _maxMembers,
-                status: _statusController.toString(),
-                type: _typeController.toString()),
-            // MyPage2Widget(),
-            // MyPage3Widget(),
-          ],
-        )
-        // ),
-        );
+      appBar: AppBar(
+        leading: BackButton(),
+        title: buildTitle(context, "Add Post"),
+      ),
+      body: PageView(
+        controller: _addPostPageController,
+        children: [
+          Page1(
+            addpostkey: _addpostkey,
+            nameController: _nameController,
+            theme: theme,
+            sportsList: sportsList,
+            purposeList: purposeList,
+            datetime: _datetime,
+            locationController: _locationController,
+            slider: slider,
+            chosenSport: _chosenSport,
+            chosenPurpose: _chosenPurpose,
+            maxMembers: _maxMembers,
+            status: _status,
+            type: _type,
+            publicRadio: publicRadio,
+            privateRadio: privateRadio,
+            teamRadio: teamRadio,
+            individualRadio: individualRadio,
+          ),
+          // MyPage2Widget(),
+          // MyPage3Widget(),
+        ],
+      ),
+    );
   }
-
-  // SimpleDialog successDialog(BuildContext context) {
-  //   return SimpleDialog(
-  //     shape: RoundedRectangleBorder(
-  //       borderRadius: BorderRadius.all(Radius.circular(20)),
-  //     ),
-  //     children: [
-  //       Center(
-  //           child: Text("Post added",
-  //               style: Theme.of(context).textTheme.headline4)),
-  //       Image.asset("assets/confirmation-illustration.png"),
-  //     ],
-  //   );
-  // }
 
   List<Widget> addPostSliverAppBar(
       BuildContext context, bool innerBoxIsScrolled) {
@@ -167,23 +187,27 @@ class _AddPostState extends State<AddPost> {
   }
 }
 
-class Page1 extends StatelessWidget {
-  const Page1(
-      {Key key,
-      @required GlobalKey<FormState> addpostkey,
-      @required TextEditingController nameController,
-      @required this.theme,
-      @required this.sportsList,
-      @required this.purposeList,
-      @required TextEditingController datetime,
-      @required TextEditingController locationController,
-      @required this.slider,
-      @required String chosenSport,
-      @required String chosenPurpose,
-      @required double maxMembers,
-      @required String type,
-      @required String status})
-      : _addpostkey = addpostkey,
+class Page1 extends StatefulWidget {
+  const Page1({
+    Key key,
+    @required GlobalKey<FormState> addpostkey,
+    @required TextEditingController nameController,
+    @required this.theme,
+    @required this.sportsList,
+    @required this.purposeList,
+    @required TextEditingController datetime,
+    @required TextEditingController locationController,
+    @required this.slider,
+    @required this.publicRadio,
+    @required this.privateRadio,
+    @required this.teamRadio,
+    @required this.individualRadio,
+    @required String chosenSport,
+    @required String chosenPurpose,
+    @required double maxMembers,
+    @required String status,
+    @required String type,
+  })  : _addpostkey = addpostkey,
         _nameController = nameController,
         _datetime = datetime,
         _locationController = locationController,
@@ -202,22 +226,32 @@ class Page1 extends StatelessWidget {
   final TextEditingController _datetime;
   final TextEditingController _locationController;
   final Slider slider;
+  final RadioListTile publicRadio;
+  final RadioListTile privateRadio;
+  final RadioListTile teamRadio;
+  final RadioListTile individualRadio;
   final String _chosenSport;
   final String _chosenPurpose;
   final double _maxMembers;
   final String _status;
   final String _type;
+
+  @override
+  _Page1State createState() => _Page1State();
+}
+
+class _Page1State extends State<Page1> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Center(
         child: Form(
-          key: _addpostkey,
+          key: widget._addpostkey,
           autovalidateMode: AutovalidateMode.onUserInteraction,
           child: Column(
             children: [
               InputBox(
-                controller: _nameController,
+                controller: widget._nameController,
                 hintText: "Event name",
                 validateFunction: Validations.validateNonEmpty,
               ),
@@ -228,11 +262,11 @@ class Page1 extends StatelessWidget {
                   padding: const EdgeInsets.symmetric(horizontal: 16.0),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(50),
-                    color: theme.currentTheme.primaryColorLight,
+                    color: Theme.of(context).inputDecorationTheme.fillColor,
                     border: Border.all(color: Color(0x00000000)),
                   ),
                   child: DropdownButtonHideUnderline(
-                    child: sportsList,
+                    child: widget.sportsList,
                   ),
                 ),
               ),
@@ -243,28 +277,53 @@ class Page1 extends StatelessWidget {
                   padding: const EdgeInsets.symmetric(horizontal: 16.0),
                   decoration: BoxDecoration(
                     borderRadius: new BorderRadius.circular(50),
-                    color: theme.currentTheme.primaryColorLight,
+                    color: Theme.of(context).inputDecorationTheme.fillColor,
                     border: Border.all(color: Color(0x00000000)),
                   ),
                   child: DropdownButtonHideUnderline(
-                    child: purposeList,
+                    child: widget.purposeList,
                   ),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: DateTimePicker(
-                  controller: _datetime,
+                  controller: widget._datetime,
                 ),
               ),
               InputBox(
-                controller: _locationController,
+                controller: widget._locationController,
                 hintText: "Location",
                 validateFunction: Validations.validateNonEmpty,
               ),
               //max member slider
-              Text("select max members"),
-              slider,
+              Divider(
+                thickness: 2,
+              ),
+              Text(
+                "Select max members",
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+              widget.slider,
+              Divider(
+                thickness: 2,
+              ),
+              Text(
+                "Select status",
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+              widget.publicRadio,
+              widget.privateRadio,
+              Divider(
+                thickness: 2,
+              ),
+              Text(
+                "Select type",
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+              widget.teamRadio,
+              widget.individualRadio,
+
               Button(
                 myText: "Add Post",
                 myColor: Theme.of(context).primaryColor,
@@ -273,16 +332,17 @@ class Page1 extends StatelessWidget {
                   // event when manually testing anything,
                   // just comment this function
                   createNewEvent(
-                      _nameController.text,
-                      userId,
-                      _locationController.text,
-                      _chosenSport,
-                      _chosenPurpose,
-                      [userId],
-                      DateTime.parse(_datetime.text),
-                      _maxMembers.toInt(),
-                      _type,
-                      _status);
+                    widget._nameController.text,
+                    userId,
+                    widget._locationController.text,
+                    widget._chosenSport,
+                    widget._chosenPurpose,
+                    [userId],
+                    DateTime.parse(widget._datetime.text),
+                    widget._maxMembers.toInt(),
+                    widget._type,
+                    widget._status,
+                  );
                   // to show success dialog
                   showDialog(
                     context: context,
@@ -295,6 +355,9 @@ class Page1 extends StatelessWidget {
                     },
                   );
                 },
+              ),
+              SizedBox(
+                height: 16,
               ),
             ],
           ),

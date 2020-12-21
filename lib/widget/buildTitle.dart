dab967b990708661b0b7930889906ac76dc4da09
Hayat Tamboli <hayat.tamboli@gmail.com>
2020-12-21 13:43:33 +0530

    added text overflow to titles

--- lib/widget/buildTitle.dart
+++ lib/widget/buildTitle.dart
@@ -3,12 +3,15 @@ import 'package:flutter/material.dart';
 Widget buildTitle(BuildContext context, String text) {
   return new Padding(
     padding: const EdgeInsets.symmetric(horizontal: 12.0),
-    child: Text(
-      text,
-      style: TextStyle(
-        fontWeight: FontWeight.w700,
-        fontSize: 24,
-        color: Theme.of(context).backgroundColor.withOpacity(0.85),
+    child: Flexible(
+      child: Text(
+        text,
+        overflow: TextOverflow.fade,
+        style: TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: 24,
+          color: Theme.of(context).backgroundColor.withOpacity(0.85),
+        ),
       ),
     ),
   );

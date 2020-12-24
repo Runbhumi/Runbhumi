43f810385f83d0fa191df576c81d7550bff2aeed
Hayat Tamboli <hayat.tamboli@gmail.com>
2020-12-24 13:59:12 +0530

    fixed parent widget err

--- lib/widget/buildTitle.dart
+++ lib/widget/buildTitle.dart
@@ -3,15 +3,13 @@ import 'package:flutter/material.dart';
 Widget buildTitle(BuildContext context, String text) {
   return new Padding(
     padding: const EdgeInsets.symmetric(horizontal: 12.0),
-    child: Flexible(
-      child: Text(
-        text,
-        overflow: TextOverflow.fade,
-        style: TextStyle(
-          fontWeight: FontWeight.w700,
-          fontSize: 24,
-          color: Theme.of(context).backgroundColor.withOpacity(0.85),
-        ),
+    child: Text(
+      text,
+      overflow: TextOverflow.fade,
+      style: TextStyle(
+        fontWeight: FontWeight.w700,
+        fontSize: 24,
+        color: Theme.of(context).backgroundColor.withOpacity(0.85),
       ),
     ),
   );

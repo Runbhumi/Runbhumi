0c24f1b05cc36e05ac216d8976a9c611f2a68707
SbangariGIT <56751089+sbangariGIT@users.noreply.github.com>
2021-01-10 17:28:39 +0530

    comments added to these files

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -1,20 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+//Start of the Meassage class
 class Message {
+  //Date and time of the message being sent
   DateTime dateTime;
+  //Id of the user who sent the message
   String sentby;
+  //What the message consists of
   String message;
+  //Name of the user who sent the message
   String sentByName;
+  //type of message, used to differentiate custom messages
   String type;
-  // List<dynamic> reaction;
 
+  //Constructor of the message class
   Message(
       {this.dateTime,
       this.sentby,
       this.message,
       this.sentByName,
       this.type}); //this.reaction});
-
+  //Method for creating a new message and assigning values to the class variables
   Message.newMessage(
     DateTime dateTime,
     String sentby,
@@ -28,6 +34,7 @@ class Message {
     // this.reaction = [0, 0, 0, 0, 0];
   }
 
+  //Converting the information to a Map to save it in the database
   Map<String, dynamic> toJson() => {
         'dateTime': dateTime,
         'sentby': sentby,
@@ -42,6 +49,7 @@ class Message {
   //       message = snapshot.data()['message'],
   //       reaction = snapshot.data()['reaction'];
 
+  // takes a query snapshot as a input and returns a the Message class
   factory Message.fromJson(QueryDocumentSnapshot data) {
     var parsedJson = data.data();
     return Message(
@@ -52,4 +60,5 @@ class Message {
         type: parsedJson['type'] ?? "");
     //    reaction: parsedJson['reaction']);
   }
+  //End of the Message class
 }
